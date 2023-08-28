67a6988a7a34a8afb5f314733f1c9f2c04ccd4dd
Jean Oliveira <99375625+jeanoliveirads@users.noreply.github.com>
2023-08-28 08:39:31 -0300

    Update exerciciofat.py

--- exerciciofat.py
+++ exerciciofat.py
@@ -11,7 +11,7 @@ Se nome e idade forem digitados:
         A primeira letra do seu nome é {letra}
         A última letra do seu nome é {letra}
 Se nada for digitado em nome ou idade: 
-    exiba "Desculpe, você deixou campos vazios."
+    exiba "Desculpe, você deixou campos vazios.
 """
 nome = input('Digite o seu nome: ')
 idade = input('Digite sua idade: ')
