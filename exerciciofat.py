a26d9b45248bdbf2d78cfbb294bca428022a9c18
Jean Oliveira <99375625+jeanoliveirads@users.noreply.github.com>
2023-09-03 18:05:09 -0300

    Update exerciciofat.py

--- exerciciofat.py
+++ exerciciofat.py
@@ -16,7 +16,7 @@ Se nada for digitado em nome ou idade:
 nome = input('Digite o seu nome: ')
 idade = input('Digite sua idade: ')
 
-if nome and idade:
+if nome and idade: 
     print(f'Seu nome é {nome}')
     print(f'Seu nome invertido é {nome[::-1]}')
 
